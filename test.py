8fb96621ca1946b21c8f84de2d05c65e9d3d6739
KS Sreeram <ks@claylabs.com>
2014-05-18 12:28:14 +0530

    Minor tweak to test.py.

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 from orderedkwargs import orderedkwargs
 
 @orderedkwargs
-def define_record(name, *fields) :
-    print "record: %s" % (name,)
-    for field_name, field_type in fields :
-        print "  field %s: %s" % (field_name, field_type)
-    print ""
+def define_record(record_name, *fields) :
+    print "record: %s" % (record_name,)
+    print "fields: %s" % (fields,)
 
-define_record("Point", x=int, y=int)
-define_record("Person", name=unicode, salary=float)
+define_record("Point", x="Int32", y="Int32")
+define_record("Person", name="Text", salary="Number")
